2a4ca6637e43b3252a8275dc707cb1d7e075d8d2
Alifer06 <alicia.fehelu@gmail.com>
2026-08-25 22:21:36 -0600

    feat: implement the interface to products

---
+++ src/interfaces/IProduct.ts
@@ -0,0 +1,10 @@
+export interface Product {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  category: string;
+  discountPercentage: number;
+  rating: number;
+  thumbnail: string;
+}
