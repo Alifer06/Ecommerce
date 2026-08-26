fe8304f8d9919e0467823314ec014209c6b98016
Alifer06 <alicia.fehelu@gmail.com>
2026-08-25 23:36:38 -0600

    feat: update ProductList to support product click events

--- src/components/Products/components/ProductList.tsx
+++ src/components/Products/components/ProductList.tsx
@@ -4,13 +4,18 @@ import { ProductCard } from './ProductCard';
 
 interface ProductListProps {
   products: Product[];
+  onProductClick: (product: Product) => void;
 }
 
-export const ProductList: React.FC<ProductListProps> = ({ products }) => {
+export const ProductList: React.FC<ProductListProps> = ({ products, onProductClick }) => {
   return (
     <div className="product-grid">
       {products.map((product) => (
-        <ProductCard key={product.id} product={product} />
+        <ProductCard
+          key={product.id}
+          product={product}
+          onClick={() => onProductClick(product)}
+        />
       ))}
     </div>
   );
